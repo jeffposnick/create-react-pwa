1d73f4f7006b45cdee13f42d1ec1e4d7b9100419
Jason Miller <jason@developit.ca>
2020-10-27 19:47:19 -0400

    remove bootstrap in favor of self-initialization

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,7 +2,5 @@ import {h, render} from 'preact';
 
 import {App} from '../components/app';
 
-export function bootstrap() {
-  render(<App></App>, document.querySelector('main'));
-  navigator.serviceWorker.register('/sw.js');
-}
+render(<App />, document.querySelector('main'));
+navigator.serviceWorker.register('/sw.js');
