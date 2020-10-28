089fbf327bccd3b019b814a51416f13a08d5f0fb
Jeff Posnick <jeffy@google.com>
2020-10-27 22:39:13 -0400

    Static

--- src/partials/end.ts
+++ src/partials/end.ts
@@ -1,5 +1,14 @@
 export function end() {
   return `</main>
-    <script src="/client/index.js"></script>
-  </body></html>`;
+    <footer>
+      <p>
+        All content comes from the Stack Exchange Network, using the
+        <a href="https://stackexchange.com/legal/api-terms-of-use">public API</a>,
+        and is licensed under
+        <a href="https://creativecommons.org/licenses/by-sa/3.0/">CC BY-SA 3.0</a>
+      </p>
+    </footer>
+    <script src="/index.js"></script>
+  </body>
+</html>`;
 }
--- src/partials/start.ts
+++ src/partials/start.ts
@@ -1,7 +1,20 @@
-export function start(title = 'Untitled Page') {
-  return `<html>
-    <head>
-      <title>${title}</title>
-    </head>
-  <body><main>`;
+export function start() {
+  return `<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Loading...</title>
+    <link rel="icon" href="/icon.png">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="theme-color" content="#0093c4">
+    <meta name="Description" content="A progressive web app for reading Stack Overflow questions and answers.">
+  </head>
+  <body>
+    <header>
+      <a href="/">SO PWA</a>
+      <a href="/about">About</a>
+    </header>
+    <img id="offline" src="/offline.svg">
+    <main>`;
 }
